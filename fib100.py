28b97996679f31ad2d751314fe137dcc7a32ce47
7Tej <150845128+7Tej@users.noreply.github.com>
2023-11-16 19:57:55 +0530

    Add files via upload

---
+++ fib100.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 16 19:53:26 2023
+
+@author: tejas
+"""
+
+from fibonacci_new import *
+
+#list l consists of the first 100 fibonacci numbers
+l= fibonacci(100)
+print(l)
